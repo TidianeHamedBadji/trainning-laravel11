52cbdef2895b5becacd5a5c8220477a7ab1cdd94
Hamed Tidiane Badji <btidiane004@gmail.com>
2025-03-15 07:29:42 +0000

    challenge Configuration Database Migration  Eloquent

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GreetingController;
+use Illuminate\Support\Facades\DB;
+use App\Models\Student;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +18,23 @@ Route::get('/about', function () {
 });
 
 Route::get('/challenge', [GreetingController::class, 'index']);
+
+
+Route::get('/models', function () {
+   //1. Using raw SQL query
+   $students = DB::select('select * from students');
+   //dd($students);
+
+    //2. Using Query Builder
+   $students = DB::table('students')->select(['nom', 'mailing'])->whereNotNull('mailing')->orderBy('nom')->get();
+  // dd($students);
+
+    //3. Using Eloquent ORM
+   //students = Student::select(['nom', 'mailing', 'status'])->whereNotNull('mailing')->get();
+    //dd($students);
+    $students   = new student();
+    $students->nom = 'Adele';
+    $students->mailing = 'adel@example.com';
+    $students->status = 'desactive';
+    $students->save();
+});
